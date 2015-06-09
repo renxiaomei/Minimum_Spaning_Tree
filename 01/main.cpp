58ed618df24e2c33d232815c1008a7da54170241
RXM <1461708116@qq.com>
2015-06-09 10:01:30 +0800

    #include<iostream> #include<limits> #include<vector> using namespace std;
    int main()
    {
    	const int a=10;
    	const int *p=&a;
    	vector<int> vi;
    	vi.em
    	cout<<*p<<endl;
    
    	return 0;
    
    }
    
    Signed-off-by: RXM <1461708116@qq.com>

---
+++ 01/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<limits>
+#include<vector>
+using namespace std;
+
+
+int main()
+{
+	const int a=10;
+	const int *p=&a;
+	vector<int> vi;
+	vi.em
+	
+
+
+	
+
+	cout<<*p<<endl;
+
+	return 0;
+
+}
